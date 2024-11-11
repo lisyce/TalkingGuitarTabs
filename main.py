a40af75a40821396a1505fb807e2e490ae982449
lisyce <lisyce@gmail.com>
2024-11-11 11:58:12 -0800

    remove scores

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from converter.tab_converter import tab_converter_parse
 
 def main():
     score = tab_converter_parse("example_scores/Dance_of_the_Moonlight_Jellies.musicxml")
-    for x in score.flatten().notes:
-        if isinstance(x, music21.chord.Chord):
-            print(x.duration.fullName, [(n.pitch.name, n.string, n.fret) for n in x.notes])   
-        else:
-            print(x.duration.fullName, [(x.pitch.name, x.string, x.fret)])
+    print(len(score.parts))
+    # for x in score.measures(1, 4):
+    #     print(x)
+        # if isinstance(x, music21.chord.Chord):
+        #     print(x.duration.fullName, [(n.pitch.name, n.string, n.fret) for n in x.notes])   
+        # else:
+        #     print(x.duration.fullName, [(x.pitch.name, x.string, x.fret)])
          
 
 if __name__=="__main__":
