b7ebf871d5e674f7bef4548244bf72c0ade24c12
lisyce <lisyce@gmail.com>
2024-11-13 19:29:18 -0800

    cleanup

--- common/measure_data.py
+++ common/measure_data.py
@@ -4,3 +4,4 @@ class MeasureData():
   def __init__(self, time_signature: str, key_signature: str, note_descs: List[str]) -> None:
     self.time_signature: str = time_signature
     self.key_signature: str = key_signature
+    self.note_descs: List[str] = note_descs
--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 import music21
 
 from converter.tab_converter import tab_converter_parse
-from needs_named import tab_part, measure_data
+from score_reader import tab_part, measure_data
 
 def main():
     score = tab_converter_parse("example_scores/Andante.musicxml")
     tabs = tab_part(score)
     
-    measure_data(tabs)
+    data = measure_data(tabs)
     
-    # for x in score.measures(1, 4):
-    #     print(x)
-        # if isinstance(x, music21.chord.Chord):
-        #     print(x.duration.fullName, [(n.pitch.name, n.string, n.fret) for n in x.notes])   
-        # else:
-        #     print(x.duration.fullName, [(x.pitch.name, x.string, x.fret)])
          
 
 if __name__=="__main__":
